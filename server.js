4822760589b310d577942ff7acdcc6d9ab930e95
John H <johnhayesru@gmail.com>
2019-12-30 02:06:33 +0300

    added mongoDB connection to server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
 
 app.use("/", (req, res) => res.json({ msg: "API Running" }));
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Listening on port ${PORT}`));
