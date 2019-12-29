def0d55a78356eeca1561fcbb8ea5ae18f05f345
John H <johnhayesru@gmail.com>
2019-12-30 02:17:38 +0300

    re-init server.js

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const express = require("express");
-const connectDB = require("./config/db");
 const app = express();
-
-// Connect Database
-connectDB();
 
 app.use("/", (req, res) => res.json({ msg: "API Running" }));
 
