47b4e3a26bae41133db8e39ef4372a627e2a4db9
John H <johnhayesru@gmail.com>
2019-12-30 02:38:52 +0300

    added all routes

--- server.js
+++ server.js
@@ -7,5 +7,11 @@ connectDB();
 
 app.use("/", (req, res) => res.json({ msg: "API Running" }));
 
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Listening on port ${PORT}`));
