df45d73aff4c4513951316d33b5f635fedb77906
John H <johnhayesru@gmail.com>
2019-12-30 02:21:07 +0300

    added params for mongoose connection

--- config/db.js
+++ config/db.js
@@ -4,7 +4,12 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useUnifiedTopology: false
+    });
 
     console.log("MongoDB Connected");
   } catch (error) {
