a94d4b5767285bf79011f71a9e08504055def6c6
John H <johnhayesru@gmail.com>
2019-12-30 02:18:43 +0300

    initialized database connection

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.use("/", (req, res) => res.json({ msg: "API Running" }));
 
