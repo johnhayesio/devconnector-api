af4f5292954d8cbfe4534c9ae85784a29c6390b8
John H <johnhayesru@gmail.com>
2019-12-30 03:05:00 +0300

    added bodyParser

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 const app = express();
 
 // Connect Database
 connectDB();
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application/json
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.json({ msg: "API Running" }));
 
