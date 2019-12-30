d85da60392f47f3a8e69482fdea54c91c76574ea
John H <johnhayesru@gmail.com>
2019-12-31 00:27:01 +0300

    added try catch to return user data with jwt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-//@router    GET api/users
+const User = require("../../models/User");
+
+//@router    GET api/auth
 //@desc      Test route
 //@access    Public
-router.get("/", (req, res) => res.json({ msg: "Auth route" }));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
 
 module.exports = router;
