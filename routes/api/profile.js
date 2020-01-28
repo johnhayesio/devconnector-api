e36a24d34f7d5295c3e191e944f31ff8829270ae
John H <johnhayesru@gmail.com>
2020-01-29 02:30:39 +0300

    added functionality to remove all post on user account deletion

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
 const Profile = require("../../models/Profile");
+const Post = require("../../models/Post");
 
 //@router    GET api/profile/me
 //@desc      Get current users profile
@@ -157,6 +158,8 @@ router.get("/user/:user_id", async (req, res) => {
 //@access    Private
 router.delete("/", auth, async (req, res) => {
   try {
+    await Post.deleteMany({ user: req.user.id });
+
     await Profile.findOneAndRemove({ user: req.user.id });
 
     await User.findOneAndRemove({ _id: req.user.id });
