4655d8371839ac42a5ab15bcc5ae68306f583b34
John H <johnhayesru@gmail.com>
2020-02-07 23:47:36 +0300

    added conversation of skills to uppercase

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -81,7 +81,9 @@ router.post(
     if (status) profileFields.status = status;
     if (githubusername) profileFields.githubusername = githubusername;
     if (skills) {
-      profileFields.skills = skills.split(",").map(skill => skill.trim());
+      profileFields.skills = skills
+        .split(",")
+        .map(skill => skill.trim().toUpperCase());
     }
 
     // Build social object
